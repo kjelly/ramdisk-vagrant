e1b65a0467942e20a2fd9f6492feeab759f47ab0
Kuo-tung Kao <ya790206@gmail.com>
2015-11-13 19:55:02 +0800

    add proxy plugins.

--- prepare.sh
+++ prepare.sh
@@ -3,5 +3,4 @@ source set-env.sh
 vagrant plugin install vagrant-hostmanager
 vagrant plugin install --plugin-version ">= 0.0.31" vagrant-libvirt
 vagrant plugin install vagrant-vbguest
-
-
+vagrant plugin install vagrant-proxyconf
