2516d7257a573c174a3bc592f573f6015fe7e1d5
Kuo-tung Kao <ya790206@gmail.com>
2015-12-18 20:26:48 +0800

    update

--- kolla/bootstrap.sh
+++ kolla/bootstrap.sh
@@ -2,3 +2,8 @@ git clone https://github.com/openstack/kolla /kolla
 chown -R vagrant:vagrant /kolla
 pip install -r /kolla/requirements.txt
 /vagrant/setup-docker.sh
+rm -f /etc/default/docker
+curl -sSL https://get.docker.io | bash
+usermod -a -G docker vagrant
+sudo apt-get install -y --force-yes docker-engine=1.8.2-0~trusty
+sudo apt-get install -y --force-yes linux-image-generic-lts-vivid
--- kolla/install.sh
+++ kolla/install.sh
@@ -1,1 +1,6 @@
-../install.sh
+#! /bin/bash
+source set-env.sh
+rm package.box
+vagrant package
+vagrant box add kolla package.box --force
+
