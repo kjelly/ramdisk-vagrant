59878cb0454911b9fb76bcd3bd072c8b5de44a3c
Kuo-tung Kao <ya790206@gmail.com>
2016-04-21 23:28:28 +0800

    update

--- prepare.sh
+++ prepare.sh
@@ -1,7 +1,9 @@
 #! /bin/bash
 source set-env.sh
-sudo apt-get install libguestfs-tools
+sudo apt-get -y --force-yes install libguestfs-tools qemu-utils libvirt-dev
+vagrant plugin install vagrant-hostmanager
 vagrant plugin install vagrant-hostmanager
 vagrant plugin install --plugin-version ">= 0.0.31" vagrant-libvirt
 vagrant plugin install vagrant-vbguest
 vagrant plugin install vagrant-proxyconf
+vagrant plugin install vagrant-mutate
